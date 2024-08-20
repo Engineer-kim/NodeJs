7707e878bfbf52b20c4e3b315e5f5c2e2e7017bc
Kimhanjin <ddang0103@naver.com>
2024-08-20 18:32:16 +0900

    몽고 DB 를 통한 주문, 상품 조회 수정 삭제 삽입 기능 추가

--- util/database.js
+++ util/database.js
@@ -1,38 +1,29 @@
-// const Sequelize = require('sequelize');
-
-// const sequelize = new Sequelize('nodejs_db', 'root', '1234', {
-//   dialect: 'mysql',
-//   host: 'localhost'
-// });
-
-// module.exports = sequelize;
-
-let _db; 
-
 const mongodb = require('mongodb');
-// const { col } = require('sequelize');
 const MongoClient = mongodb.MongoClient;
- 
-const mongoConnect = (callback) => {
-  MongoClient.connect('mongodb+srv://kimhanjin:75kYKRBxracYLrj6@cluster0.0bms1.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster0')
-  .then(client => {
-    console.log("connected");
-    _db = client.db();
-    callback();
-  }).catch(err =>{
-    console.log(err);
-    throw err;
-  });
+
+let _db;
+
+const mongoConnect = callback => {
+  MongoClient.connect(
+    'mongodb+srv://kimhanjin:75kYKRBxracYLrj6@cluster0.0bms1.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster0'
+  )
+    .then(client => {
+      console.log('Connected!');
+      _db = client.db();
+      callback();
+    })
+    .catch(err => {
+      console.log(err);
+      throw err;
+    });
 };
 
-const  getDb = () => {
-  if(_db){
+const getDb = () => {
+  if (_db) {
     return _db;
   }
-    throw 'NO DB';
-}
-
-module.exports = {
-  mongoConnect,
-  getDb
+  throw 'No database found!';
 };
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
