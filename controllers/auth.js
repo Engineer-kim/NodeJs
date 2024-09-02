8c493c7fff6c63b19a42d79765598d7b88b2930c
Kimhanjin <ddang0103@naver.com>
2024-09-02 11:40:32 +0900

    Feature: 사용자가 입력한 값 삭제되지 않게 유지
    회원가입시 조건에 부합하지 않은 정보로 회원가입시 기존에는 정보가 삭제 -> 정보 유지로 변경

--- controllers/auth.js
+++ controllers/auth.js
@@ -50,7 +50,11 @@ exports.getSignup = (req, res, next) => {
   res.render('auth/signup', {
     path: '/signup',
     pageTitle: 'Signup',
-    errorMessage: message
+    errorMessage: message,
+    oldInput: { email: '', 
+      password: '', 
+      confirmPassword: ''
+      }
   });
 };
 
@@ -94,7 +98,11 @@ exports.postSignup = (req, res, next) => {
     return res.status(422).render(res.render('auth/signup', {
       path: '/signup',
       pageTitle: 'Signup',
-      errorMessage: errors.array()[0].msg
+      errorMessage: errors.array()[0].msg,
+      oldInput: { email: email, 
+        password: password, 
+        confirmPassword: req.body.confirmPassword
+        }
       })
     );
   }   
