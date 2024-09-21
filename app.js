5923dc3e4f6cf2e1ff2dd2159d791a805f4e6a0c
Kimhanjin <ddang0103@naver.com>
2024-09-21 16:41:49 +0900

    Fix: 읽는 부분 삭제

--- app.js
+++ app.js
@@ -30,8 +30,8 @@ const store = new MongoDBStore({
 });
 const csrfProtection = csrf();
 
-const privateKey = fs.readFileSync('server.key');
-const certificate = fs.readFileSync('server.cert');
+// const privateKey = fs.readFileSync('server.key');
+// const certificate = fs.readFileSync('server.cert');
 
 const fileStorage = multer.diskStorage({
   destination: (req, file, cb) => {  // 파일 업로드 될때의 저장소
